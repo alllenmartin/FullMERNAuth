a138a4640970a08dcfa51674c2ad790acf800520
alllenmartin <martinallen722@gmail.com>
2022-06-29 12:52:07 +0300

    db connection

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connection = require('./db');
+
+//database connection
+connection();
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 8012;
-app.listen(port, ()=> console.log('Listening to port ${port}'))
+app.listen(port, () => console.log(`Listening to port ${port} `))
