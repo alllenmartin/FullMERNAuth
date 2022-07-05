c6e73bc0151fe1eed7d7c238bd44d3d62675f984
alllenmartin <martinallen722@gmail.com>
2022-07-05 18:04:32 +0300

    fdsfds

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,41 +3,42 @@ const {User, validate } = require("../models/user");
 const bcrypt = require("bcrypt")
 const Token = require('../models/token')
 const sendEmail = require('../utils/sendEmail');
+const crypto = require("crypto");
 //const { generateOTP } = require("../utils/sendEmail");
 
-router.post('/', async (req, res) => {
-    try {
-        const {error} = validate(req.body);
+router.post("/", async (req, res) => {
+	try {
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
 
-        if(error)
-            return res.status(400).send({message:error.details[0].message});
-            
+		let user = await User.findOne({ email: req.body.email });
+		if (user)
+			return res
+				.status(409)
+				.send({ message: "User with given email already Exist!" });
 
-        let user = await User.findOne({email: req.body.email})
-        if(user)
-        return res.status(409).send({message:"Email Already Exists"});
+		const salt = await bcrypt.genSalt(Number(process.env.SALT));
+		const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-        const salt = await bcrypt.genSalt(Number(process.env.SALT))
-        const hashPassword = await bcrypt.hash(req.body.password, salt)
+		user = await new User({ ...req.body, password: hashPassword }).save();
 
-        user = await new User({...req.body, password:hashPassword}).save();
-
-        const OTP = generateOTP();
-
-        const token = await new Token({
-            userId: user._id,
-            token: OTP,
-        }).save();
-        await sendEmail(user.email, 'Verification Code', token)
-        res.status(201).send({message:"An email sent to your account. Please verify"});
         
-        
-    } catch (error) {
-       
-        console.log(error)
-        res.status(500).send({message:"Internal Server Error"});
-       
-    }
-})
+
+		const token = await new Token({
+			userId: user._id,
+			token: crypto.randomBytes(32).toString("hex"),
+		}).save();
+		const url = `${process.env.BASE_URL}users/${user.id}/verify/${token.token}`;
+		await sendEmail(user.email, "Verify Email", token);
+
+		res
+			.status(201)
+			.send({ message: "An Email sent to your account please verify" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
 
 module.exports= router;
